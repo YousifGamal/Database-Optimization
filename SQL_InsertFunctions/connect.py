2a3824ae6e1230ed27a4b4885d150e879700b9b8
Khaled Galal <44265417+Gellesh@users.noreply.github.com>
2021-01-11 18:43:15 +0200

    add connect to gitignore

--- SQL_InsertFunctions/connect.py
+++ SQL_InsertFunctions/connect.py
@@ -4,10 +4,14 @@ import mysql.connector
 def ConnectMySQL():
     mydb = mysql.connector.connect(
         host="localhost",
-        user="newuser",
-        password="password",
+        user="Gellesh",
+        password="khaledG123",
         database="college"
     )
     #print(mydb.autocommit)
     
     return mydb
+
+
+
+ConnectMySQL()
